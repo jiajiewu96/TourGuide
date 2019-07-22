eecd542d75fd9b16cc9aa83c604c62c0db6e7688
wackieju <jiajie.wu96@gmail.com>
2019-07-22 02:04:04 -0700

    Added tab layout

--- app/src/main/java/com/example/tourguide/CategoryAdapter.java
+++ app/src/main/java/com/example/tourguide/CategoryAdapter.java
@@ -1,18 +1,34 @@
 package com.example.tourguide;
 
+import android.content.Context;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
+    private Context mContext;
 
-    public CategoryAdapter(FragmentManager fm) {
+    public CategoryAdapter(FragmentManager fm, Context context) {
         super(fm);
+        mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.category_attractions);
+            default:
+                return mContext.getString(R.string.category_attractions);
+        }
     }
 
     @Override
     public Fragment getItem(int position) {
-        switch (position){
+        switch (position) {
             case 0:
                 return new AttractionsFragment();
             default:
--- app/src/main/java/com/example/tourguide/MainActivity.java
+++ app/src/main/java/com/example/tourguide/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,8 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager(), this);
         viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 }
